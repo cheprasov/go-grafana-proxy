bd681715c7291aa0511bf7352b1e689fccae5730
Alexander Cheprasov <acheprasov84@gmail.com>
2021-03-25 16:10:34 +0000

    Upd

--- cmd/grafana-proxy.go
+++ cmd/grafana-proxy.go
@@ -1,147 +1,151 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "go-grafana-proxy/pkg/config"
-    "go-grafana-proxy/pkg/grafana"
-    "log"
-    "strconv"
-    "time"
+	"flag"
+	"fmt"
+	"go-grafana-proxy/pkg/config"
+	"go-grafana-proxy/pkg/grafana"
+	"log"
+	"strconv"
+	"time"
 
-    "github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp"
 )
 
+var allowedMetricTypes = []string{"temperature", "humidity"}
+
 var cfg config.Config
+var lastMetrics = make(map[string]grafana.Metric)
 
 func init() {
-    configFilePointer := flag.String("config", "", "Path to config.json file")
-    flag.Parse()
+	configFilePointer := flag.String("config", "", "Path to config.json file")
+	flag.Parse()
 
-    var err error
-    cfg, err = config.ReadConfig(*configFilePointer)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	cfg, err = config.ReadConfig(*configFilePointer)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println(cfg)
+	fmt.Println(cfg)
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    h := requestHandler
-    if cfg.Compress {
-        h = fasthttp.CompressHandler(h)
-    }
+	h := requestHandler
+	if cfg.Compress {
+		h = fasthttp.CompressHandler(h)
+	}
 
-    if err := fasthttp.ListenAndServe(cfg.Listen, h); err != nil {
-        log.Fatalf("Error in ListenAndServe: %s", err)
-    }
+	if err := fasthttp.ListenAndServe(cfg.Listen, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
 }
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
 
-    fmt.Println(ctx, &ctx.Request)
+	// fmt.Println(ctx, &ctx.Request)
 
-    switch string(ctx.RequestURI()) {
-    case "/grafana-proxy":
-        go routeGrafanaProxy(ctx)
-    default:
-        go routeDefault(ctx)
-    }
+	switch string(ctx.RequestURI()) {
+	case "/grafana-proxy":
+		routeGrafanaProxy(ctx)
+	case "/metrics":
+		routeLastMetrics(ctx)
+	default:
+		routeDefault(ctx)
+	}
 
-    // If the file doesn't exist, create it, or append to the file
-    //f, err := os.OpenFile("access.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-    //if err != nil {
-    //    log.Fatal(err)
-    //}
-    //defer f.Close()
-    //if _, err := fmt.Fprint(f, &ctx.Request); err != nil {
-    //    log.Fatal(err)
-    //}
+	// If the file doesn't exist, create it, or append to the file
+	//f, err := os.OpenFile("access.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	//if err != nil {
+	//    log.Fatal(err)
+	//}
+	//defer f.Close()
+	//if _, err := fmt.Fprint(f, &ctx.Request); err != nil {
+	//    log.Fatal(err)
+	//}
 }
 
 func routeDefault(ctx *fasthttp.RequestCtx) {
-    //fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-    //fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-    //fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-    //fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-    //fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-    //fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-    //fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-    //fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-    //fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-    //fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+	//fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+	//fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+	//fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+	//fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+	//fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+	//fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+	//fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+	//fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+	//fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+	//fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
 
-    fmt.Fprint(ctx, &ctx.Request)
-    ctx.SetContentType("text/plain; charset=utf8")
+	// fmt.Fprint(ctx, &ctx.Request)
+	ctx.SetContentType("text/plain; charset=utf8")
 
-    // Set arbitrary headers
-    // ctx.Response.Header.Set("X-My-Header", "my-header-value")
+	// Set arbitrary headers
+	// ctx.Response.Header.Set("X-My-Header", "my-header-value")
 
-    // Set cookies
-    //var c fasthttp.Cookie
-    //c.SetKey("cookie-name")
-    //c.SetValue("cookie-value")
-    //ctx.Response.Header.SetCookie(&c)
+	// Set cookies
+	//var c fasthttp.Cookie
+	//c.SetKey("cookie-name")
+	//c.SetValue("cookie-value")
+	//ctx.Response.Header.SetCookie(&c)
 }
 
 func routeError(ctx *fasthttp.RequestCtx) {
-    ctx.Response.SetStatusCode(400)
+	ctx.Response.SetStatusCode(400)
+}
+
+func routeLastMetrics(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("text/plain; charset=utf8")
+	for _, metric := range lastMetrics {
+		fmt.Fprintln(ctx, metric.Sensor, metric.Type, ":", metric.Value)
+	}
 }
 
 func routeGrafanaProxy(ctx *fasthttp.RequestCtx) {
-    if string(ctx.Method()) != "POST" {
-        routeError(ctx)
-        return
-    }
-    authKey := string(ctx.Request.Header.Peek("Authorization"))
-    if string(authKey) != cfg.Authorization {
-        routeError(ctx)
-        return
-    }
+	if string(ctx.Method()) != "POST" {
+		routeError(ctx)
+		return
+	}
+	authKey := string(ctx.Request.Header.Peek("Authorization"))
+	if string(authKey) != cfg.Authorization {
+		routeError(ctx)
+		return
+	}
 
-    name := string(ctx.PostArgs().Peek("name"))
+	name := string(ctx.PostArgs().Peek("name"))
 
-    if len(name) == 0 {
-        routeError(ctx)
-        return
-    }
+	if len(name) == 0 {
+		routeError(ctx)
+		return
+	}
 
-    fmt.Fprintf(ctx, "Request method is %q\n")
+	var metrics = make([]grafana.Metric, 0, 2)
 
-    var metrics = make([]grafana.Metric, 0, 2)
+	for _, metricType := range allowedMetricTypes {
+		stringMetric := string(ctx.PostArgs().Peek(metricType))
+		if len(stringMetric) != 0 {
+			value, err := strconv.ParseFloat(stringMetric, 64)
+			if err == nil {
+				metric := grafana.Metric{
+					Sensor:   name,
+					Type:     metricType,
+					Name:     "data.flat." + name + "." + metricType,
+					Value:    value,
+					Time:     time.Now().Unix(),
+					Interval: 60,
+				}
+				metrics = append(metrics, metric)
+				lastMetrics[metric.Name] = metric
+			}
+		}
+	}
 
-    temperature := string(ctx.PostArgs().Peek("temperature"))
-    if len(temperature) != 0 {
-        value, err := strconv.ParseFloat(temperature, 64)
-        if err == nil {
-            metrics = append(metrics, grafana.Metric{
-                Name:     "data.flat." + name + ".temperature",
-                Value:    value,
-                Time:     time.Now().Unix(),
-                Interval: 60,
-            })
-        }
-    }
+	if len(metrics) == 0 {
+		routeError(ctx)
+		return
+	}
 
-    humidity := string(ctx.PostArgs().Peek("humidity"))
-    if len(humidity) != 0 {
-        value, err := strconv.ParseFloat(humidity, 64)
-        if err == nil {
-            metrics = append(metrics, grafana.Metric{
-                Name:     "data.flat." + name + ".humidity",
-                Value:    value,
-                Time:     time.Now().Unix(),
-                Interval: 60,
-            })
-        }
-    }
-
-    if len(metrics) == 0 {
-        routeError(ctx)
-        return
-    }
-    grafana.PostMetrics(cfg.PostUrl, cfg.ApiKey, metrics)
+	grafana.PostMetrics(cfg.PostUrl, cfg.ApiKey, metrics)
 }
--- pkg/grafana/grafanaPost.go
+++ pkg/grafana/grafanaPost.go
@@ -1,41 +1,43 @@
 package grafana
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/valyala/fasthttp"
+	"encoding/json"
+	"fmt"
+	"github.com/valyala/fasthttp"
 )
 
 type Metric struct {
-    Name  string  `json:"name"`
-    Value float64 `json:"value"`
-    Time  int64   `json:"time"`
-    Interval int  `json:"interval"`
+	Sensor   string  `json:"-"`
+	Type     string  `json:"-"`
+	Name     string  `json:"name"`
+	Value    float64 `json:"value"`
+	Time     int64   `json:"time"`
+	Interval int     `json:"interval"`
 }
 
 func PostMetrics(url string, apikey string, metrics []Metric) {
-    postData, _ := json.Marshal(metrics)
-    fmt.Println(string(postData))
+	postData, _ := json.Marshal(metrics)
+	fmt.Println(string(postData))
 
-    // Put these in the global scope so they don't get converted for each request.
+	// Put these in the global scope so they don't get converted for each request.
 
-    req := fasthttp.AcquireRequest()
-    req.Header.SetMethod("POST")
-    req.Header.SetContentType("application/json")
-    req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apikey))
-    req.SetRequestURI(url)
-    req.SetBody(postData)
+	req := fasthttp.AcquireRequest()
+	req.Header.SetMethod("POST")
+	req.Header.SetContentType("application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apikey))
+	req.SetRequestURI(url)
+	req.SetBody(postData)
 
-    res := fasthttp.AcquireResponse()
-    if err := fasthttp.Do(req, res); err != nil {
-        panic("handle error")
-    }
-    fasthttp.ReleaseRequest(req)
+	res := fasthttp.AcquireResponse()
+	if err := fasthttp.Do(req, res); err != nil {
+		panic("handle error")
+	}
+	fasthttp.ReleaseRequest(req)
 
-    body := res.Body()
+	body := res.Body()
 
-    fmt.Println(string(body))
-    // Do something with body.
+	fmt.Println(string(body))
+	// Do something with body.
 
-    fasthttp.ReleaseResponse(res) // Only when you are done with body!
+	fasthttp.ReleaseResponse(res) // Only when you are done with body!
 }
